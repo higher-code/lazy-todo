6af985cbcd69748ead3cd586cc8afb01c8dabdf4
qvil <qvil1127@gmail.com>
2018-03-17 16:16:06 +0900

    [Firebase] user authentication

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from 'components';
 import registerServiceWorker from './registerServiceWorker';
+// Firebase
+import * as firebase from 'firebase';
+// Custom
+import { App } from 'components';
+/**
+ * @Warning Config not managed by git
+ */
+import users from 'config/users'; 
+import firebaseConfig from 'config/firebase';
+
+firebase.initializeApp(firebaseConfig);
+
+let email = users.qvil.email;
+let password = users.qvil.password;
+
+// Login
+firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
+  let errorCode = error.code;
+  let errorMessage = error.message;
+
+  console.error(errorCode, errorMessage);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
